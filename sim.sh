996ea48198492c3b88eb54ada88bd2f069280e61
Tang Piseth <tangpiseth43@gmail.com>
2024-03-10 00:19:03 +1030

    Completed Mux and Dmux Chips -- Making progresses

---
+++ sim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if a filename is provided as an argument
+if [ -z "$1"]; then
+	echo "Please provide a test script file name ending in .tst"
+	exit 1
+fi
+
+# Run the HardwareSimulator.sh script with the provided test script's filename
+sh ~/COMP_SCI_2000/tools/HardwareSimulator.sh prac1/"$1"
