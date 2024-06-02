e994acf43b68648e35fdd9a45a53f6920c01b04c
Tang Piseth <tangpiseth43@gmail.com>
2024-06-02 11:44:21 +0930

    Completed some tutorials on regular expressions.

--- RegularExpression.cpp
+++ RegularExpression.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <string>
 #include <regex>
 
 
 
 int main(int argc, char** argv){
-    std::regex h;
+    // std::string expression = "softwareTesting";
+    // std::string matchString = "(soft)(.*)"; 
+    // std::regex matcher(matchString);
+    // bool match = std::regex_match(expression, matcher);
+    // if (match) std::cout << "<" << expression << "> matches <" <<  matchString << "> !" << std::endl;
+    // else std::cout << "Something went wrong" << std::endl;
+    
+    if (std::regex_match("softwareTesting", std::regex("(soft)(.*)"))) std::cout << "string:literal => matched" << std::endl;
+    
+
+    // 
+    const char mystr[]  = "SoftwareTestingHelp";
+    std::string str("software");
+    std::regex str_expr("(soft)(.*)");
+    if (std::regex_match(mystr, str_expr)) std::cout << "string:obj => matched" << std::endl;
+
+    if (std::regex_match(str.begin(), str.end(), str_expr)) std::cout << "str:range(begin-end) => matched" << std::endl;
+    
+
+    std::cmatch cm;
+    std::regex_match(mystr, cm,str_expr);
+
+    std::smatch sm;
+    std::regex_match(str, sm, str_expr);
+
+
     
 
 
